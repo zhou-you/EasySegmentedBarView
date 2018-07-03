360a592dea137a9e5d37bdfffe08c73afbba75ad
zhou-you <zhouyou1989123@gmail.com>
2018-07-03 16:32:30 +0800

    增加关于

--- app/src/main/java/com/zhouyou/view/segmentedbar/demo/MainActivity.java
+++ app/src/main/java/com/zhouyou/view/segmentedbar/demo/MainActivity.java
@@ -21,6 +21,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -39,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            //actionBar.setHomeButtonEnabled(true);
+            //actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.openOptionsMenu();
+        }
+        
         //setTitle("分段组件(SegmentedBarView)");
         javaCodeLayout = (LinearLayout) findViewById(R.id.java_code_layout);
         sideStyleLayout = (LinearLayout) findViewById(R.id.side_style_layout);
@@ -68,6 +78,25 @@ public class MainActivity extends AppCompatActivity {
         createBarViewWithOilSliderType5();
         createBarViewWithOilSliderType6();
         createBarthumb4();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.adout_layout:
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //通过xml配置BarView
